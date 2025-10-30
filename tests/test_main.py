e7bf5d447d9719e11d76c39074462be6373f84b6
JoaoSantos407 <joao.silva40@outlook.com.br>
2025-10-30 20:00:20 -0300

    Ajuste de saudação

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,7 @@
+# tests/test_main.py
+
 from app.main import greet
 
 def test_greet_basic():
-    assert greet("DevOps") == "Olá, DevOps!"
+    # Altere o valor esperado aqui:
+    assert greet("DevOps") == "Olá, DevOps! Bem-vindo à Aula 05."
